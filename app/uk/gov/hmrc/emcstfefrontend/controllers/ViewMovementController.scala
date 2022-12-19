2d77995a5778b8e460b88006e339a14daf658de2
Stephen Ward <15212062+stephenkward@users.noreply.github.com>
2022-12-19 13:30:42 +0000

    Removed service as not needed this stage

--- app/uk/gov/hmrc/emcstfefrontend/controllers/ViewMovementController.scala
+++ app/uk/gov/hmrc/emcstfefrontend/controllers/ViewMovementController.scala
@@ -7,24 +7,21 @@ package uk.gov.hmrc.emcstfefrontend.controllers
 
 import javax.inject.{Inject, Singleton}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.emcstfefrontend.services.ViewMovementService
 import uk.gov.hmrc.emcstfefrontend.views.html.ViewMovementPage
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ViewMovementController @Inject()(
                                       mcc: MessagesControllerComponents,
-                                      service: ViewMovementService,
                                       viewMovementPage: ViewMovementPage,
                                       implicit val executionContext: ExecutionContext)
   extends FrontendController(mcc) {
 
-  def viewMovement(): Action[AnyContent] = Action.async { implicit request =>
-    service.getMovement map {
-      case() => Ok(viewMovementPage())
+  def viewMovement(): Action[AnyContent] = Action.async {
+    implicit request => {
+      Future.successful(Ok(viewMovementPage()))
     }
   }
-
 }
