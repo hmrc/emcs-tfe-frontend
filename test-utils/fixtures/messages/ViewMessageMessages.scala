4ff5372def57db4088bba4fb1064656c28e04790
grahampaulcook <grahampaulcook@users.noreply.github.com>
2024-02-15 11:49:32 +0000

    ETFE-3363 Adjusted COD link text (UX approved)

--- test-utils/fixtures/messages/ViewMessageMessages.scala
+++ test-utils/fixtures/messages/ViewMessageMessages.scala
@@ -30,7 +30,7 @@ object ViewMessageMessages {
     val deleteMessageLinkText = "Delete message"
     val reportOfReceiptLinkText = "Submit report of receipt"
     val explainDelayLinkText = "Create explanation for a delay and provide information"
-    val changeDestinationLinkText = "Change of destination"
+    val changeDestinationLinkText = "Submit new change of destination"
   }
 
   sealed trait IE810SubmissionFailureMessages { _: i18n =>
