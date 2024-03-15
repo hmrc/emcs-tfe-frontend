01f728fcd6ebdbe894f58d5d827b88e4aa696727
grahampaulcook <grahampaulcook@users.noreply.github.com>
2024-03-15 11:12:42 +0000

    ETFE-3566 Adding missing test

---
+++ test/controllers/cod/ChangeOfDestinationControllerSpec.scala
@@ -0,0 +1,72 @@
+package controllers.cod
+
+import base.SpecBase
+import config.AppConfig
+import controllers.predicates.{BetaAllowListActionImpl, FakeAuthAction, FakeDataRetrievalAction}
+import mocks.config.MockAppConfig
+import mocks.connectors.MockBetaAllowListConnector
+import models.requests.DataRequest
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, convertToStringShouldWrapper}
+import play.api.http.Status
+import play.api.i18n.{Messages, MessagesApi}
+import play.api.mvc.{AnyContentAsEmpty, MessagesControllerComponents, Result}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{redirectLocation, status}
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ChangeOfDestinationControllerSpec extends SpecBase with FakeAuthAction with MockFactory with MockBetaAllowListConnector with MockAppConfig {
+
+  class Test(navHubEnabled: Boolean = true, codEnabled: Boolean = true) {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val ec: ExecutionContext = app.injector.instanceOf[ExecutionContext]
+    implicit val fakeRequest: DataRequest[AnyContentAsEmpty.type] = dataRequest(FakeRequest("GET", "/"))
+    implicit val messages: Messages = app.injector.instanceOf[MessagesApi].preferred(fakeRequest)
+
+    lazy val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+
+    lazy val betaAllowListAction = new BetaAllowListActionImpl(
+      betaAllowListConnector = mockBetaAllowListConnector,
+      errorHandler = errorHandler,
+      config = mockAppConfig
+    )
+
+    val controller: ChangeOfDestinationController = new ChangeOfDestinationController(
+      app.injector.instanceOf[MessagesControllerComponents],
+      FakeSuccessAuthAction,
+      new FakeDataRetrievalAction(testMinTraderKnownFacts, testMessageStatistics),
+      betaAllowListAction
+    )(ec, appConfig)
+
+    MockedAppConfig.betaAllowListCheckingEnabled.repeat(2).returns(true)
+    MockBetaAllowListConnector.check(testErn, "tfeNavHub").returns(Future.successful(Right(navHubEnabled)))
+    MockBetaAllowListConnector.check(testErn, "tfeChangeDestination").returns(Future.successful(Right(codEnabled)))
+  }
+
+  "GET /trader/:ern/movement/:arc/version/:ver/change-destination" when {
+
+    "user is on the private beta list" should {
+
+      "return under construction" in new Test {
+        val result: Future[Result] = controller.onPageLoad(testErn, testArc, 1)(fakeRequest)
+
+        status(result) mustBe Status.SEE_OTHER
+        redirectLocation(result) shouldBe Some("http://localhost:8319/emcs/change-destination/trader/GBWKTestErn/movement/ARC")
+      }
+    }
+
+    "user is NOT on the private beta list" should {
+
+      "redirect to legacy at a glance page" in new Test(codEnabled = false) {
+        val result: Future[Result] = controller.onPageLoad(testErn, testArc, 3)(fakeRequest)
+
+        status(result) mustBe Status.SEE_OTHER
+        redirectLocation(result) shouldBe Some(s"http://localhost:8080/emcs/trader/$testErn/movement/$testArc/version/3/changedestination")
+      }
+    }
+
+  }
+
+}
