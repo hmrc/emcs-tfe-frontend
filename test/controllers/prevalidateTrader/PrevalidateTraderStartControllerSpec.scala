e2ef968c0cf687b2c7e19caf2484cc74c2a68406
Graham Cook <grahampaulcook@users.noreply.github.com>
2024-04-03 09:40:31 +0100

    Update test/controllers/prevalidateTrader/PrevalidateTraderStartControllerSpec.scala
    Co-authored-by: Lewis Mitchell <53828896+LewisMitchell@users.noreply.github.com>

--- test/controllers/prevalidateTrader/PrevalidateTraderStartControllerSpec.scala
+++ test/controllers/prevalidateTrader/PrevalidateTraderStartControllerSpec.scala
@@ -82,7 +82,7 @@ class PrevalidateTraderStartControllerSpec
           val result = controller.onPageLoad(testErn)(request)
 
           status(result) mustEqual SEE_OTHER
-          redirectLocation(result) mustBe Some("http://localhost:8080/emcs/trader/GBWKTestErn/prevalidate")
+          redirectLocation(result).value mustEqual "http://localhost:8080/emcs/trader/GBWKTestErn/prevalidate"
         }
       }
 
